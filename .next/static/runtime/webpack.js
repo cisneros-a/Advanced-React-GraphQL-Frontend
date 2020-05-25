8d1005a1d5e91f4aca32ef3e2a03b6743cf67d45
github username <github email address>
2020-05-24 19:11:50 -0500

    added functionality to permissions table

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "dbad8d79dc0fe85565d3";
+/******/ 	var hotCurrentHash = "0c0eb135fde32f602e03";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
